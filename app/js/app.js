1bba950498774d0ace76e3571cc18104bbf69ef3
helbert-rocha <helberti@outlook.com>
2016-04-02 14:38:04 -0300

    First commit

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var myApp = angular.module('myApp', [
+  'ngMaterial',
+  'ui.router'
+]).
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  //
+  // Now set up the states
+  $stateProvider
+      .state('home', {
+        url: "/",
+        templateUrl: "partials/home.html"
+      });
+});
